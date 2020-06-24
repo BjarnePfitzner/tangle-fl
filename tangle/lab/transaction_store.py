Fix ray object store overflow

tangle/lab/transaction_store.py
@@ -6,28 +6,29 @@ import numpy as np
 from ..core import TransactionStore
 
 class FilesystemTransactionStore:
-    def __init__(self, tx_path):
+    def __init__(self, tangle_path, tx_path):
+        self.tangle_path = tangle_path
         self.tx_path = tx_path
 
-    def load_transaction_weights(self, tx):
-        return np.load(f'{self.tx_path}/{tx.name()}.npy', allow_pickle=True)
+    def load_transaction_weights(self, tx_id):
+        return np.load(f'{self.tx_path}/{tx_id}.npy', allow_pickle=True)
 
-    def compute_transaction_id(self, tx):
+    def compute_transaction_id(self, tx_weights):
         tmpfile = io.BytesIO()
-        self._save(tx, tmpfile)
+        self._save(tx_weights, tmpfile)
         tmpfile.seek(0)
         return self.hash_file(tmpfile)
 
-    def save(self, tx):
-        tx.id = self.compute_transaction_id(tx)
+    def save(self, tx, tx_weights):
+        tx.id = self.compute_transaction_id(tx_weights)
 
         os.makedirs(self.tx_path, exist_ok=True)
 
-        with open(f'{self.tx_path}/{tx.name()}.npy', 'wb') as tx_file:
-            self._save(tx, tx_file)
+        with open(f'{self.tx_path}/{tx.id}.npy', 'wb') as tx_file:
+            self._save(tx_weights, tx_file)
 
-    def _save(self, tx, file):
-        np.save(file, tx.weights, allow_pickle=True)
+    def _save(self, tx_weights, file):
+        np.save(file, tx_weights, allow_pickle=True)
 
     @staticmethod
     def hash_file(f):

tangle/ray-lab/main.py
@@ -4,8 +4,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import ray
 
 from ..lab import Lab, LabConfiguration, ModelConfiguration, parse_args
+# from ..core import Tangle
 
 class RayLab(Lab):
+    def __init__(self, config, model_config):
+        super().__init__(config, model_config)
+
+        self.remote_train_data = {
+            cid : ray.put(data) for (cid, data) in self.train_data.items()
+        }
+        self.remote_test_data = {
+            cid : ray.put(data) for (cid, data) in self.test_data.items()
+        }
+
     def create_genesis(self):
 
         @ray.remote
@@ -21,13 +32,24 @@ class RayLab(Lab):
 
             # Suppress tf warnings
             tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+            # tangle = Tangle.fromfile(tx_store.tangle_path, round-1)
  
             return Lab.create_node_transaction(tangle, round, client_id, cluster_id, train_data, eval_data, seed, model_config, tx_store)
+            # if tx is not None:
+            #     tx.weights = ray.put(tx.weights)
+            #     return tx
+            # return None
 
-        futures = [_create_node_transaction.remote(tangle, round, client_id, cluster_id, self.train_data[client_id], self.test_data[client_id], self.config.seed, self.model_config, self.tx_store)
+        futures = [_create_node_transaction.remote(tangle, round, client_id, cluster_id, self.remote_train_data[client_id], self.remote_test_data[client_id], self.config.seed, self.model_config, self.tx_store)
                    for (client_id, cluster_id) in clients]
 
         return ray.get(futures)
+        # for tx in txs:
+        #     if tx is not None:
+        #         tx.weights = ray.get(tx.weights)
+
+        # return txs
 
 def main():
     args = parse_args()