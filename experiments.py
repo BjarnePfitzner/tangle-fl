5d48c178532fdd3d160d4091283c165d52273dba
Paul Geppert <Paul.Geppert@Student.HPI.de>
2020-07-21 19:20:34 +0000

    Fixed experiments.py

--- experiments.py
+++ experiments.py
@@ -13,35 +13,27 @@ from sklearn.model_selection import ParameterGrid
 ############################# Parameter section #############################
 #############################################################################
 
-data_generation_path = "leaf/data/synthetic/"
-data_generation_commands = ["rm -r data", "rm -r meta", "python main.py -num-tasks 1000 -num-classes 5 -num-dim 60 --prob-clusters 0.2 0.3 0.3 0.2"]
-data_preprocessing_commands = ["bash ./preprocess.sh -s niid --sf 1.0 -k 5 -t sample --tf 0.85"]
-
 params = {
-    'dataset': ['synthetic'],   # is expected to be one value to construct default experiment name
-    'model': ['log_reg'],       # is expected to be one value to construct default experiment name
-    'num_rounds': [200],
+    'dataset': ['femnist'],   # is expected to be one value to construct default experiment name
+    'model': ['cnn'],       # is expected to be one value to construct default experiment name
+    'num_rounds': [10],
     'eval_every': [-1],
     'clients_per_round': [10],
+    'model_data_dir': ['../data/femnist-data/small'],
+    'tip_selector': ['accuracy'],
     'num_tips': [2],
     'sample_size': [2],
     'reference_avg_top': [1],
     'target_accuracy': [1],
     'learning_rate':  [0.005],
-    'poison_type': ['NONE'],
+    'poison_type': ['none'],
     'poison_fraction': [0],
     'poison_from': [1],
-    'acc-tip-selection-strategy': ['WALK'],
-    'acc-cumulate-ratings': ['False', 'True'],
-    'acc-ratings-to-weights': ['LINEAR'],
-    'acc-select-from-weights': ['WEIGHTED_CHOICE'],
-    'acc-alpha': [0.001]
-}
-
-tip_selector_start_rounds = {
-    'tip_selector': [],
-    'acc_tip_selector': [],
-    'mal_tip_selector': []
+    'acc_tip_selection_strategy': ['WALK'],
+    'acc_cumulate_ratings': ['False', 'True'],
+    'acc_ratings_to_weights': ['LINEAR'],
+    'acc_select_from_weights': ['WEIGHTED_CHOICE'],
+    'acc_alpha': [0.001],
 }
 
 ##############################################################################
@@ -49,11 +41,10 @@ tip_selector_start_rounds = {
 ##############################################################################
 
 def main():
-    traings_data_output_filename = 'data_generation.log'
     setup_filename = '1_setup.log'
     console_output_filename = '2_training.log'
 
-    #exit_if_repo_not_clean()
+    # exit_if_repo_not_clean()
 
     args = parse_args()
     experiment_folder = prepare_exp_folder(args)
@@ -61,9 +52,6 @@ def main():
     print("[Info]: Experiment results and log data will be stored at %s" % experiment_folder)
 
     git_hash = get_git_hash()
-    cwd = get_current_working_directory()
-    generate_and_preprocess_data(experiment_folder + '/' + traings_data_output_filename)
-    change_working_directory_to(cwd)
     run_and_document_experiments(args, experiment_folder, setup_filename, console_output_filename, git_hash)
 
 def exit_if_repo_not_clean():
@@ -73,7 +61,7 @@ def exit_if_repo_not_clean():
         dirty_files, errs = proc.communicate(timeout=3)
     except subprocess.TimeoutExpired:
         proc.kill()
-        outs, errs = proc.communicate()
+        _, errs = proc.communicate()
         print('[Error]: Could not check git status!: %s' % errs, file=sys.stderr)
         exit(1)
     
@@ -130,37 +118,10 @@ def get_git_hash():
         git_hash = git_hash.decode("utf-8")
     except subprocess.TimeoutExpired:
         proc.kill()
-        outs, errs = proc.communicate()
+        _, errs = proc.communicate()
         git_hash = 'Could not get Githash!: %s' % errs
     
     return git_hash
-
-def get_current_working_directory():
-    return os.getcwd()
-
-def generate_and_preprocess_data(traings_data_output_file):
-    with open(traings_data_output_file, 'w+') as file:
-        print('Data generation commands: %s' % data_generation_commands, file=file)
-        print('Data preprocessing commands: %s' % data_preprocessing_commands, file=file)
-        print('', file=file)
-
-    with open(traings_data_output_file, 'a+') as file:
-        # change to data generation dir
-        os.chdir(data_generation_path)
-
-        print('Generating data...')
-        for command in data_generation_commands:
-            generation = subprocess.Popen(command.split(" "), stdout=file, stderr=file)
-            generation.wait()
-
-
-        print('Preprocessing data...')
-        for command in data_preprocessing_commands:
-            preprocessing = subprocess.Popen(command.split(" "), stdout=file, stderr=file)
-            preprocessing.wait()
-
-def change_working_directory_to(working_directory):
-    os.chdir(working_directory)
 
 def run_and_document_experiments(args, experiments_dir, setup_filename, console_output_filename, git_hash):
     parameter_grid = ParameterGrid(params)
@@ -171,59 +132,56 @@ def run_and_document_experiments(args, experiments_dir, setup_filename, console_
         os.makedirs(experiment_folder, exist_ok=True)
 
         # Prepare execution command
-        command = 'python main.py ' \
+        command = 'python -m tangle.ray ' \
             '-dataset %s ' \
             '-model %s ' \
             '--num-rounds %s ' \
             '--eval-every %s ' \
             '--clients-per-round %s ' \
+            '--tangle-dir %s ' \
+            '--model-data-dir %s ' \
+            '--target-accuracy %s ' \
             '--num-tips %s ' \
             '--sample-size %s ' \
-            '--reference-avg-top %s ' \
-            '--target-accuracy %s ' \
             '-lr %s ' \
-            '--poison-type %s ' \
-            '--poison-fraction %s ' \
-            '--poison-from %s '\
+            '--reference-avg-top %s ' \
+            '--tip-selector %s ' \
             '--acc-tip-selection-strategy %s ' \
             '--acc-cumulate-ratings %s ' \
             '--acc-ratings-to-weights %s ' \
             '--acc-select-from-weights %s ' \
-            '--tangle-dir %s'
+            '--acc-alpha %s ' \
+            '--poison-type %s ' \
+            '--poison-fraction %s ' \
+            '--poison-from %s'
         parameters = (
             p['dataset'],
             p['model'],
             p['num_rounds'],
             p['eval_every'],
             p['clients_per_round'],
+            experiment_folder + '/tangle_data',
+            p['model_data_dir'],
+            p['target_accuracy'],
             p['num_tips'],
             p['sample_size'],
-            p['reference_avg_top'],
-            p['target_accuracy'],
             p['learning_rate'],
+            p['reference_avg_top'],
+            p['tip_selector'],
+            p['acc_tip_selection_strategy'],
+            p['acc_cumulate_ratings'],
+            p['acc_ratings_to_weights'],
+            p['acc_select_from_weights'],
+            p['acc_alpha'],
             p['poison_type'],
             p['poison_fraction'],
-            p['poison_from'],
-            p['acc-tip-selection-strategy'],
-            p['acc-cumulate-ratings'],
-            p['acc-ratings-to-weights'],
-            p['acc-select-from-weights'],
-            experiment_folder)
+            p['poison_from'])
         command = command % parameters
-
-        for start_round in tip_selector_start_rounds['tip_selector']:
-            command += ' --tip-selector-from %s' %start_round
-        for start_round in tip_selector_start_rounds['acc_tip_selector']:
-            command += ' --acc-tip-selector-from %s' %start_round
-        for start_round in tip_selector_start_rounds['mal_tip_selector']:
-            command += ' --mal-tip-selector-from %s' %start_round
 
         start_time = datetime.datetime.now()
 
         # Print Parameters and command
         with open(experiment_folder + '/' + setup_filename, 'w+') as file:
-            print('Data generation commands: %s' % data_generation_commands, file=file)
-            print('Data preprocessing commands: %s' % data_preprocessing_commands, file=file)
             print('', file=file)
             print('StartTime: %s' % start_time, file=file)
             print('Githash: %s' % git_hash, file=file)
