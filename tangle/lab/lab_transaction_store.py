340743d3e3954ef3b41a74a4cc13bc77f3a60b1a
Paul Geppert <Paul.Geppert@Student.HPI.de>
2020-10-13 15:12:28 +0200

    Extended LabTransactionStore to separate load and save of data

--- tangle/lab/lab_transaction_store.py
+++ tangle/lab/lab_transaction_store.py
@@ -7,12 +7,37 @@ import numpy as np
 from ..core import TransactionStore, Transaction, Tangle
 
 class LabTransactionStore(TransactionStore):
-    def __init__(self, tangle_path):
-        self.tangle_path = tangle_path
-        self.tx_path = os.path.join(tangle_path, 'transactions')
+    """
+    A LabTransactionStore allows to save and load tangles and transactions.
+    """
+
+    def __init__(self, dest_tangle_path, src_tangle_path = None):
+        """
+        Parameters:
+        -----------
+            `dest_tangle_path`: str
+                The path used to store tangles and transactions to.
+            `src_tangle_path`: str
+                The path used to load pre-existing tangles and transactions from (default: dest_tangle_path)
+        """
+        self.dest_tangle_path = dest_tangle_path
+        self.dest_tx_path = os.path.join(dest_tangle_path, 'transactions')
+
+        if src_tangle_path is not None:
+            self.src_tangle_path = src_tangle_path
+            self.src_tx_path = os.path.join(src_tangle_path, 'transactions')
+        else:
+            self.src_tangle_path = self.dest_tangle_path
+            self.src_tx_path = self.dest_tx_path
 
     def load_transaction_weights(self, tx_id):
-        return np.load(f'{self.tx_path}/{tx_id}.npy', allow_pickle=True)
+        # If the transaction belongs to the pre-existing tangle, load it from there,
+        # otherwise we generated it and we have to load it from the destination path
+        if os.path.exists(f'{self.src_tx_path}/{tx_id}.npy'):
+            return np.load(f'{self.src_tx_path}/{tx_id}.npy', allow_pickle=True)
+        else:
+            return np.load(f'{self.dest_tx_path}/{tx_id}.npy', allow_pickle=True)
+        foo = LabTransactionStore()
 
     def compute_transaction_id(self, tx_weights):
         tmpfile = io.BytesIO()
@@ -23,9 +48,9 @@ class LabTransactionStore(TransactionStore):
     def save(self, tx, tx_weights):
         tx.id = self.compute_transaction_id(tx_weights)
 
-        os.makedirs(self.tx_path, exist_ok=True)
+        os.makedirs(self.dest_tx_path, exist_ok=True)
 
-        with open(f'{self.tx_path}/{tx.id}.npy', 'wb') as tx_file:
+        with open(f'{self.dest_tx_path}/{tx.id}.npy', 'wb') as tx_file:
             self._save(tx_weights, tx_file)
 
     def _save(self, tx_weights, file):
@@ -44,20 +69,26 @@ class LabTransactionStore(TransactionStore):
         return sha1.hexdigest()
 
     def save_tangle(self, tangle, tangle_name):
-        os.makedirs(self.tangle_path, exist_ok=True)
+        os.makedirs(self.dest_tangle_path, exist_ok=True)
 
         n = [{'id': t.id,
               'parents': list(t.parents),
               'metadata': t.metadata } for _, t in tangle.transactions.items()]
 
-        with open(f'{self.tangle_path}/tangle_{tangle_name}.json', 'w') as outfile:
+        with open(f'{self.dest_tangle_path}/tangle_{tangle_name}.json', 'w') as outfile:
             json.dump({'nodes': n, 'genesis': tangle.genesis}, outfile, indent=4)
 
         tangle.name = tangle_name
 
     def load_tangle(self, tangle_name):
-        with open(f'{self.tangle_path}/tangle_{tangle_name}.json', 'r') as tanglefile:
-            t = json.load(tanglefile)
+        # If the tangle belongs to the pre-existing tangle(s), load it from there,
+        # otherwise we generated it and we have to load it from the destination path
+        if os.path.exists(f'{self.src_tangle_path}/tangle_{tangle_name}.json'):
+            with open(f'{self.src_tangle_path}/tangle_{tangle_name}.json', 'r') as tanglefile:
+                t = json.load(tanglefile)
+        else:
+            with open(f'{self.dest_tangle_path}/tangle_{tangle_name}.json', 'r') as tanglefile:
+                t = json.load(tanglefile)
 
         transactions = {n['id']: Transaction(
                                     set(n['parents']),
