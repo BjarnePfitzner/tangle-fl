264f58e5e09487fedaf7ae0621bcefefbbc496f4
Robert Schmid <r.schmid@outlook.com>
2020-10-30 12:33:10 +0000

    Use lazy tip selector

--- tangle/ray/ray_tip_selector_factory.py
+++ tangle/ray/ray_tip_selector_factory.py
@@ -2,6 +2,7 @@ import random
 import ray
 
 from ..core.tip_selection.accuracy_tip_selector import AccuracyTipSelectorSettings
+from ..core.tip_selection.lazy_accuracy_tip_selector import LazyAccuracyTipSelector
 from ..lab import TipSelectorFactory, Lab
 from ..models.baseline_constants import ACCURACY_KEY
 
@@ -14,6 +15,7 @@ class RayTipSelectorFactory(TipSelectorFactory):
 
     def create(self, tangle, dataset, client_id, tx_store):
         if self.config.tip_selector == 'accuracy':
+
             tip_selection_settings = {}
             tip_selection_settings[AccuracyTipSelectorSettings.SELECTION_STRATEGY] = self.config.acc_tip_selection_strategy
             tip_selection_settings[AccuracyTipSelectorSettings.CUMULATE_RATINGS] = self.config.acc_cumulate_ratings
@@ -21,23 +23,32 @@ class RayTipSelectorFactory(TipSelectorFactory):
             tip_selection_settings[AccuracyTipSelectorSettings.SELECT_FROM_WEIGHTS] = self.config.acc_select_from_weights
             tip_selection_settings[AccuracyTipSelectorSettings.ALPHA] = self.config.acc_alpha
 
-            rayAccuracyTipSelector = RayAccuracyTipSelector(tangle, tip_selection_settings)
+            return LazyAccuracyTipSelector(tangle, tip_selection_settings)
 
-            if self.config.acc_tip_selection_strategy == 'GLOBAL' and not self.config.acc_cumulate_ratings:
-                txs_to_eval = rayAccuracyTipSelector.tips
-            else:
-                txs_to_eval = tangle.transactions.keys()
-            futures = [self.compute_accuracy_ratings.remote(self, client_id, tx_id, random.randint(0, 4294967295), dataset.model_config, dataset.remote_train_data[client_id], tx_store) for tx_id in txs_to_eval]
-            currents = ray.get(futures)
-            rayAccuracyTipSelector.add_precomputed_ratings({tx_id: r for tx_id, r, _ in currents})
+            # tip_selection_settings = {}
+            # tip_selection_settings[AccuracyTipSelectorSettings.SELECTION_STRATEGY] = self.config.acc_tip_selection_strategy
+            # tip_selection_settings[AccuracyTipSelectorSettings.CUMULATE_RATINGS] = self.config.acc_cumulate_ratings
+            # tip_selection_settings[AccuracyTipSelectorSettings.RATINGS_TO_WEIGHT] = self.config.acc_ratings_to_weights
+            # tip_selection_settings[AccuracyTipSelectorSettings.SELECT_FROM_WEIGHTS] = self.config.acc_select_from_weights
+            # tip_selection_settings[AccuracyTipSelectorSettings.ALPHA] = self.config.acc_alpha
 
-            for tx_id, accuracy, node_id in currents:
-                if node_id is not None:
-                    if node_id not in self.accuracy_cache:
-                        self.accuracy_cache[node_id] = {}
-                    self.accuracy_cache[node_id][tx_id] = accuracy
+            # rayAccuracyTipSelector = RayAccuracyTipSelector(tangle, tip_selection_settings)
 
-            return rayAccuracyTipSelector
+            # if self.config.acc_tip_selection_strategy == 'GLOBAL' and not self.config.acc_cumulate_ratings:
+            #     txs_to_eval = rayAccuracyTipSelector.tips
+            # else:
+            #     txs_to_eval = tangle.transactions.keys()
+            # futures = [self.compute_accuracy_ratings.remote(self, client_id, tx_id, random.randint(0, 4294967295), dataset.model_config, dataset.remote_train_data[client_id], tx_store) for tx_id in txs_to_eval]
+            # currents = ray.get(futures)
+            # rayAccuracyTipSelector.add_precomputed_ratings({tx_id: r for tx_id, r, _ in currents})
+
+            # for tx_id, accuracy, node_id in currents:
+            #     if node_id is not None:
+            #         if node_id not in self.accuracy_cache:
+            #             self.accuracy_cache[node_id] = {}
+            #         self.accuracy_cache[node_id][tx_id] = accuracy
+
+            # return rayAccuracyTipSelector
 
         return super().create(tangle)
 
