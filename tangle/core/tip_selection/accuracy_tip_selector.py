b647cdc160d0b4157b76441580d546f2f739198c
Robert Schmid <r.schmid@outlook.com>
2020-11-22 19:28:46 +0000

    Multiply accuracies with future-set based ratings

--- tangle/core/tip_selection/accuracy_tip_selector.py
+++ tangle/core/tip_selection/accuracy_tip_selector.py
@@ -40,6 +40,10 @@ class AccuracyTipSelector(TipSelector):
         for tx_id in txs:
             rating[tx_id] = node.test(node.tx_store.load_transaction_weights(tx_id), 'train')[ACCURACY_KEY]
 
+        future_set_cache = {}
+        for tx in txs:
+            rating[tx] *= len(TipSelector.future_set(tx, self.approving_transactions, future_set_cache)) + 1
+
         return rating
 
     def compute_ratings(self, node):
