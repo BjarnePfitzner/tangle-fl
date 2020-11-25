7daf7f6f139059d4f6ea5bd3f75509119b67e762
Jossekin Beilharz <beilharz@acm.org>
2020-11-25 15:03:00 +0100

    Add Averaged Accuracy in Viewer

--- experiments.py
+++ experiments.py
@@ -22,7 +22,7 @@ params = {
     'eval_every': [99],
     'eval_on_fraction': [0.05],
     'clients_per_round': [10],
-    'model_data_dir': ['./tangle/data/nextcharacter/data'],
+    'model_data_dir': ['../data/poets-8/data'],
     'src_tangle_dir': [''],         # Set to '' to not use --src-tangle-dir parameter
     'start_round': [2],
     'tip_selector': ['default'],
