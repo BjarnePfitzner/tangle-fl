0d5277ccce86a7d0ea02ce2f2a4bc2ee37c2f80d
Paul Geppert <paul.geppert@student.hpi.de>
2020-12-21 11:20:20 +0100

    Fix for RayAccuracyTipSelector

--- tangle/ray/ray_accuracy_tip_selector.py
+++ tangle/ray/ray_accuracy_tip_selector.py
@@ -8,7 +8,7 @@ class RayAccuracyTipSelector(AccuracyTipSelector):
     def add_precomputed_ratings(self, precomputed_ratings):
         self.precomputed_ratings = precomputed_ratings
 
-    def _compute_ratings(self, node):
+    def _compute_ratings(self, node, tx=None):
         if self.precomputed_ratings is None:
             raise RuntimeError('Variable precomputed_ratings has to be set manually by calling add_precomputed_ratings()')
         return self.precomputed_ratings
